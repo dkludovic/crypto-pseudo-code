de88bdbb627858d453eb115f8bba9c0c87d60327
dkludovic <ludodk60@gmail.com>
2016-11-12 15:43:14 +0100

    Update pseudo-code.c

--- pseudo-code.c
+++ pseudo-code.c
@@ -1,18 +1,20 @@
 /* Generate a RSA key */
 
-int a, b;
+int p, q, a, b;
 
+/* calculate p as a random prime */
 while (e divides b) { // e divides (p - 1)
 	rand_prime(p, sizeofKey / 2, indexPRNG);
 	a = p - 1;
 	b = gcd(a, e);
 }
 
+/* calculate p as a random prime */
 while (e divides b) { // e divides (q - 1)
 	rand_prime(q, sizeofKey / 2, indexPRNG);
 	a = q - 1;
 	b = gcd(a, e);
 }
 
-N = p x q;
-d = 1/e mod [lcm(p – 1) (q – 1)];
+key->N = p x q;
+key->d = 1/e mod [lcm(p – 1) (q – 1)];
